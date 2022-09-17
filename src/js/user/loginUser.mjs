a3593962ff03b4af775ac0fb1c6090d10835501e
pretzL <ericpretzinger@gmail.com>
2022-09-17 09:33:42 +0200

    Created working login authentication which returns the user info payload

--- src/js/user/loginUser.mjs
+++ src/js/user/loginUser.mjs
@@ -1,7 +1,7 @@
 import { API_BASE_URL } from "../util/variables.mjs";
 
 // Connect to the correct form later
-const form = document.querySelector(".register-form");
+const form = document.querySelector(".login-form");
 
 // Eventlistener for the submit handling
 form.addEventListener("submit", handleSubmit);
@@ -11,26 +11,23 @@ function handleSubmit(evt) {
   evt.preventDefault();
 
   // Assign the inputs from the form to variables
-  const [userName, email, password, avatar, banner] = evt.target.elements;
+  const [email, password] = evt.target.elements;
 
   // Construct the data object which is to be sent to the API
   const dataObj = JSON.stringify({
-    name: userName.value,
     email: email.value,
     password: password.value,
-    avatar: avatar.value,
-    banner: banner.value,
   });
 
   // Send the data object to the API
-  fetch(`${API_BASE_URL}/api/v1/social/auth/register`, {
+  fetch(`${API_BASE_URL}/api/v1/social/auth/login`, {
     method: "POST",
     body: dataObj,
     headers: {
       "Content-Type": "application/json; charset=utf-8",
     },
   })
-    .then((response) => response.json)
+    .then((response) => response.json())
     .then((json) => console.log(json))
     .catch((error) => console.log("error", error));
 }
