113492d11b153b20c32d1b22bdccd5907f921313
pretzL <ericpretzinger@gmail.com>
2022-09-19 17:42:45 +0200

    Added default value for user profile images

--- src/js/pages/index.mjs
+++ src/js/pages/index.mjs
@@ -11,10 +11,19 @@ const userInfo = getUserInfo();
 const profileImage = document.querySelectorAll(".profile-image");
 const userName = document.querySelectorAll(".profile-info-username");
 
+// Check if an image is set for the user, if not set to default
+let userAvatar = userInfo.avatar;
+
+if (!userAvatar || userAvatar === "string" || userAvatar === "https://img.service.com/avatar.jpg") {
+  userAvatar = "/dist/img/no-user-image-icon-0.jpg";
+}
+
+// Add the image
 profileImage.forEach((image) => {
-  image.innerHTML = `<img class="profile-image-attributes rounded-circle" src="${userInfo.avatar}" alt="User Profile Image" />`;
+  image.innerHTML = `<img class="profile-image-attributes rounded-circle" src="${userAvatar}" alt="User Profile Image" />`;
 });
 
+// Add the name
 userName.forEach((name) => {
   name.innerText = `${userInfo.name}`;
 });
