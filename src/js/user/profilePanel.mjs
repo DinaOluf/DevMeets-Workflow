98f08b01daf923738a1bce1855f74944b74ab100
pretzL <ericpretzinger@gmail.com>
2022-09-25 17:44:47 +0200

    Revert "Refactoring/fixing"

--- src/js/user/profilePanel.mjs
+++ src/js/user/profilePanel.mjs
@@ -1,5 +1,4 @@
 import { getUserInfo } from "../user/getUserInfo.mjs";
-import { bannerImageHandler, profileImageHandler } from "../components/imageHandlers.mjs";
 
 export function getUserPanelInfo() {
   // Get user info and display it
@@ -11,7 +10,11 @@ export function getUserPanelInfo() {
   const bannerImage = document.querySelector(".banner-image");
 
   // Check if an image is set for the user, if not set to default
-  let userAvatar = profileImageHandler(userInfo.avatar);
+  let userAvatar = userInfo.avatar;
+
+  if (!userAvatar || userAvatar === "string" || userAvatar === "https://img.service.com/avatar.jpg") {
+    userAvatar = "/dist/img/no-user-image-icon-0.jpg";
+  }
 
   // Add the image
   profileImage.forEach((image) => {
@@ -20,7 +23,11 @@ export function getUserPanelInfo() {
 
   if (bannerImage) {
     // Check if a banner image is set for the user, if not set to default
-    let userBanner = bannerImageHandler(userInfo.banner);
+    let userBanner = userInfo.banner;
+
+    if (!userBanner || userBanner === "string" || userBanner === "https://img.service.com/banner.jpg") {
+      userBanner = "/dist/img/Hero-Banner-Placeholder-Dark-1024x480-1.png";
+    }
 
     // Add the image
     bannerImage.innerHTML = `<img class="img-fluid" src="${userBanner}" alt="User Banner Image" />`;
