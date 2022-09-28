aff0bdd0c2338a4c5956b942337057c0eb8db7db
pretzL <ericpretzinger@gmail.com>
2022-09-28 11:33:34 +0200

    Initial braindump comment about what needs to be done

---
+++ src/js/user/getProfile.mjs
@@ -0,0 +1,3 @@
+// Get the profile dynamically through URL Search Params. Present the data in the existing HTML and if API.name matches
+// localStorage.name, make follow button an edit profile button instead. Edit profile will be handled in a different branch.
+// As will follow/unfollow function.
