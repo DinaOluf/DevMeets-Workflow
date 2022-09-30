7c8a631f8351f5f2522e49660e373776d8e60d69
pretzL <ericpretzinger@gmail.com>
2022-09-30 17:04:59 +0200

    Removed comment

--- src/js/user/profiles/getProfile.mjs
+++ src/js/user/profiles/getProfile.mjs
@@ -1,7 +1,3 @@
-// Get the profile dynamically through URL Search Params. Present the data in the existing HTML and if API.name matches
-// localStorage.name, make follow button an edit profile button instead. Edit profile will be handled in a different branch.
-// As will follow/unfollow function.
-
 import { API_BASE_URL, API_PROFILE_URL } from "/src/js/util/variables.mjs";
 import { options } from "/src/js/util/options.mjs";
 import { buildProfile } from "./buildProfile.mjs";
