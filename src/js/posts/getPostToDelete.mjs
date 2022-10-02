75354eec1ca85301da1b3a0c51d7d296ab146699
Sindre1993 <sindre.e.ostby@hotmail.com>
2022-10-02 17:48:50 +0200

    Updated comments

--- src/js/posts/getPostToDelete.mjs
+++ src/js/posts/getPostToDelete.mjs
@@ -3,12 +3,17 @@ import { deletePost } from "./deletePost.mjs";
 export function getPostToDelete() {
   const deletePostButton = document.querySelectorAll(".delete-post");
 
+  // Clicking the Delete button in the cogwheel, targets the closest post and grabs the ID
+  // and feeds it into the deletePost function.
+
   deletePostButton.forEach((e) => {
     e.addEventListener("click", (i) => {
       const closestPost = i.target.closest(".card-body");
       const postId = closestPost.childNodes[3].innerText;
 
       deletePost(postId);
+
+      // Reloads the website upon clicking delete.
       setTimeout(location.reload.bind(location), 1000);
     });
   });
