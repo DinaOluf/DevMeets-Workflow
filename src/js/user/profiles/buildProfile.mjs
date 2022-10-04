c0738c3e76226f3f2443f7b57e3e0c0529a1c0c5
pretzL <ericpretzinger@gmail.com>
2022-10-04 15:15:33 +0200

    Trying to add eventlisteners, no dice yet. Will continue later

--- src/js/user/profiles/buildProfile.mjs
+++ src/js/user/profiles/buildProfile.mjs
@@ -2,7 +2,8 @@ import { bannerImageHandler, profileImageHandler } from "/src/js/components/imag
 import { getUserInfo } from "../getUserInfo.mjs";
 
 export function buildProfile(data) {
-  const profileImage = document.querySelectorAll(".profile-image");
+  const profileImage = document.querySelector(".profile-image");
+  const editProfileImg = document.querySelector(".editable-profile-image");
   const userName = document.querySelectorAll(".profile-info-username");
   const bannerImage = document.querySelector(".banner-image");
   const follow = document.querySelector(".follow");
@@ -11,21 +12,41 @@ export function buildProfile(data) {
   let userAvatar = profileImageHandler(data.avatar);
 
   // Add the image
-  profileImage.forEach((image) => {
-    image.innerHTML = `<img class="profile-image-attributes rounded-circle" src="${userAvatar}" alt="User Profile Image" />`;
-  });
+  editProfileImg.innerHTML = `<img class="profile-image-attributes rounded-circle avatar-to-hover" src="${userAvatar}" alt="User Profile Image" />
+                      <a href="#!" class="align-items-center me-3 profile-edit-avatar">&#9998; Edit</a>`;
 
-  if (bannerImage) {
+  profileImage.innerHTML = `<img class="profile-image-attributes rounded-circle" src="${userAvatar}" alt="User Profile Image" />`;
+
+  if (data.banner) {
     // Check if a banner image is set for the user, if not set to default
     let userBanner = bannerImageHandler(data.banner);
 
     // Add the image
-    bannerImage.innerHTML = `<img class="img-fluid" src="${userBanner}" alt="User Banner Image" />`;
+    bannerImage.innerHTML = `<img class="img-fluid banner-to-hover" src="${userBanner}" alt="User Banner Image" />
+                            <a href="#!" class="align-items-center me-3 profile-edit-banner">&#9998; Edit</a>`;
   }
 
   // Add the name
   userName.forEach((name) => {
     name.innerText = `${data.name}`;
+  });
+
+  // Set up eventListeners for hover states
+  const avatarHover = document.querySelector(".avatar-to-hover");
+  const editAvatar = document.querySelector(".profile-edit-avatar");
+
+  console.log(avatarHover);
+  console.log(editAvatar);
+
+  avatarHover.addEventListener("mouseover", () => {
+    editAvatar.style.display = "block";
+  });
+
+  const bannerHover = document.querySelector(".banner-to-hover");
+  const editBanner = document.querySelector(".profile-edit-avatar");
+
+  bannerHover.addEventListener("mouseover", () => {
+    editBanner.style.display = "block";
   });
 
   // Check if name matches saved name in localStorage, if so change button to be edit instead of follow
@@ -35,6 +56,7 @@ export function buildProfile(data) {
     follow.innerHTML = "Edit profile";
   }
 
+  // Set eventListeners for both clicking follow and clicking edit
   if (follow.innerHTML === "Follow") {
     follow.addEventListener("click", () => {
       console.log("Following mode");
