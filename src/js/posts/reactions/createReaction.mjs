e291b6d20eb288ddfe464e82131fce1435c511d8
Sindre1993 <sindre.e.ostby@hotmail.com>
2022-10-08 22:37:45 +0200

    Made plan for work

---
+++ src/js/posts/reactions/createReaction.mjs
@@ -0,0 +1,7 @@
+// Fetch request : PUT /api/v1/social/posts/<id>/react/<symbol>, where symbol HAS to be a thumbs up!
+
+// Function arguments (id, 👍).
+
+// Add reaction.length to display amount of reactions besides like button.
+
+// postTemple add querySelector for button and add eventlistener "click" for createReaction function.
