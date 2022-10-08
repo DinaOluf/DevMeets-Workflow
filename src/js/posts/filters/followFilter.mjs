610a277674763f7f2dcb8266ef52e2e6eb80ad6d
pretzL <ericpretzinger@gmail.com>
2022-10-08 21:14:31 +0200

    Filter on following now works

---
+++ src/js/posts/filters/followFilter.mjs
@@ -0,0 +1,10 @@
+import { getItem } from "../../user/getUserInfo.mjs";
+
+const follow = JSON.parse(getItem("follow"));
+
+// Sort passed in array if it includes posts by people user is following
+export function sortFollow(posts) {
+  const followerNames = follow.map((follower) => follower.name);
+  const sortedArray = posts.filter((post) => followerNames.includes(post.author.name));
+  return sortedArray;
+}
