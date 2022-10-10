ac6afd836f1aec8e7c70a77b66b7cc00c09a0140
pretzL <ericpretzinger@gmail.com>
2022-10-10 14:22:31 +0200

    When creating a post, tags are now split at "," and are added as an array to the POST request.

--- src/js/posts/createPost.mjs
+++ src/js/posts/createPost.mjs
@@ -10,11 +10,13 @@ export function createPost(evt) {
   // Assign the inputs from the form to variables
   const [title, body, tags, media] = evt.target.elements;
 
+  const pushedTags = tags.value.replace(/\s+/g, "").split(",");
+
   // Construct the data object which is to be sent to the API
   let dataObj = {
     title: `${title.value}`,
     body: `${body.value}`,
-    tags: [`${tags.value}`],
+    tags: pushedTags,
     media: `${media.value}`,
   };
 
