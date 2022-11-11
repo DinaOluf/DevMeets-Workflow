3eb9c6f5279a84674b025d9bc418d8ed2226f919
Dina <dina.olufsen@hotmail.com>
2022-11-11 12:31:15 +0100

    Cypress links to run on deployed GitHub Page

--- cypress/e2e/testing/DevMeets.cy.js
+++ cypress/e2e/testing/DevMeets.cy.js
@@ -7,7 +7,6 @@ describe("DevMeets", () => {
 
   beforeEach(() => {
     cy.restoreLocalStorage();
-    cy.visit("http://127.0.0.1:5501");
   });
 
   afterEach(() => {
@@ -15,7 +14,7 @@ describe("DevMeets", () => {
   });
 
   it("can log in with valid user inputs", () => {
-    cy.visit("http://127.0.0.1:5501/login.html");
+    cy.visit("https://dinaoluf.github.io/DevMeets-Workflow/login.html");
     cy.url().should("include", "login");
     cy.get("input[id='login-email']").should("exist").type("dina@noroff.no");
     cy.get("input[id='login-password']")
@@ -27,7 +26,7 @@ describe("DevMeets", () => {
   });
 
   it("can create a post on the API", () => {
-    cy.visit("http://127.0.0.1:5501/index.html");
+    cy.visit("https://dinaoluf.github.io/DevMeets-Workflow/index.html");
     cy.url().should("include", "index");
     cy.get("input[id='post-title']").should("exist").type("Hello You");
     cy.get("textarea[id='post-content']")
@@ -37,14 +36,12 @@ describe("DevMeets", () => {
     cy.wait(5000);
     cy.get(".post-body")
       .eq(0)
-      .should(
-        "have.text",
-        "\n            Thanks for looking at my tests!\n            "
-      );
+      .contains("Thanks for looking at my tests!")
+      .should("exist");
   });
 
   it("can delete a post from the API", () => {
-    cy.visit("http://127.0.0.1:5501/index.html");
+    cy.visit("https://dinaoluf.github.io/DevMeets-Workflow/index.html");
     cy.url().should("include", "index");
     cy.get("svg[id='cog-dropdown']").should("exist").eq(0).click();
     cy.get("li[class='dropdown-item delete-post']")
@@ -59,7 +56,7 @@ describe("DevMeets", () => {
   });
 
   it("can log out", () => {
-    cy.visit("http://127.0.0.1:5501/index.html");
+    cy.visit("https://dinaoluf.github.io/DevMeets-Workflow/index.html");
     cy.url().should("include", "index");
     cy.get("svg[id='settings-dropdown']").should("exist").click();
     cy.get("li[class='dropdown-item log-out-button']").should("exist").click();
